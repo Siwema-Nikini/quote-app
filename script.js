357b179a379e4f477eebb583e8f598a722c28f8c
siwema <siwema@localhost.localdomain>
2023-02-28 13:22:55 +0300

    after adding fetch id by name

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+
+// Arrays are used to store multiple values in a single variable.
+// API(application programming interface is mechanis which enable two software component to communicate using set of definitions and protocols)
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitteBtn = document.getElementById("twitter");
+const NewQuoteBtn = document.getElementById("new-quote");
 let apiQuotes=[];
 
 function newQuote(){
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent= quote.text;
+    
 }
 
 async function getQuote(){
